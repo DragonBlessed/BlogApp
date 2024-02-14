b2ca1ecccb6b55b6de5862dd2e65940019ef6f5f
DragonBlessed <christianvasil72@gmail.com>
2024-02-14 08:09:28 -0800

    Added function that prints whatever user types in the input field onto a box in the center of the window.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use slint::Weak;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+    let ui_handle: Weak<AppWindow> = ui.as_weak();
+    ui.on_posted(move | string | {
+        let ui: AppWindow = ui_handle.unwrap();
+        let posted_text = string;
+        ui.set_postText(posted_text.into());
+    });
 
     ui.run()
 }
